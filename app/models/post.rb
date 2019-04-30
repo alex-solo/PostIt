c3c737d1699c415ca6b87fccd9ab56be79c11e74
Alex Soloviev <alex.soloviev@gmail.com>
2019-04-29 23:11:07 -0400

    second commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'   #foreign_key and class_name not strictly necessary here if we stuck to convention. from 
                                                                  # convention and say belongs_to 'user' Rails will  expect foreign_key 'user_id' and class name User. So we can just leave out the two parameters
 
-  has_many :comments                                                               
+  has_many :comments
+  has_many :post_categories
+  has_many :categories, through: :post_categories                                                          
 end
